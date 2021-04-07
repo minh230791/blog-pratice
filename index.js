f8d95c516a30ead663bbe673c5abd230f170f71a
Mac <ifix@Vus-MacBook-Pro.local>
2021-04-07 13:25:09 +0700

    Structure folder

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// set view engine (default: pug)
+app.set('view engine', 'ejs');
+// set views folder
+app.set('views', './views');
+
+// set static folder
+app.use('/public', express.static('./public'));
+
+// routes
+
+
+
 app.listen(3000, () => {
     console.log('Running...');
 });
