f1544b930485c5a4b667a858e6093c4325547f1a
Mac <ifix@Vus-MacBook-Pro.local>
2021-04-07 15:50:12 +0700

    Create new index.ejs

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
-
+const engine = require('ejs-locals')
 const app = express();
 
 // set view engine (default: pug)
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
+
 // set views folder
-app.set('views', './views');
+app.set('views',__dirname + '/views');
 
 // set static folder
 app.use('/public', express.static('./public'));
 
 // routes
 
-
+app.get('/', (req, res) => {
+    res.render('./admin/pages/index')
+})
 
 app.listen(3000, () => {
     console.log('Running...');
